991aa47f8a687b30bdafbfdf3ed86350310cfc13
mrbergis <mrbergis@mail.ru>
2023-04-17 20:05:33 +0300

    5. Animation Logics

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,6 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MoveDirection
+{
+    Down,
+    Up,
+    Left,
+    Right
+}
 public class PlayerLogic : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
@@ -13,14 +20,20 @@ public class PlayerLogic : MonoBehaviour
     Vector2 _movementInput;
     Vector2 _movementVelocity;
     
+    MoveDirection _movementDirection = MoveDirection.Down;
+    
+    Animator _animator;
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     
     void Update()
     {
         UpdateMovementInput();
+        UpdateMovementDirection();
     }
     
     void UpdateMovementInput()
@@ -30,8 +43,44 @@ public class PlayerLogic : MonoBehaviour
 
         _movementInput = new Vector2(_horizontalInput, _verticalInput);
         _movementVelocity = _movementInput.normalized * _movementSpeed;
+        
+        if(_animator)
+        {
+            _animator.SetFloat("MovementInput", _movementInput.magnitude);
+        }
     }
 
+    void UpdateMovementDirection()
+    {
+        if (Mathf.Abs(_horizontalInput) > Mathf.Abs(_verticalInput))
+        {
+            if (_horizontalInput > 0)
+            {
+                _movementDirection = MoveDirection.Right;
+            }
+            else if (_horizontalInput < 0)
+            {
+                _movementDirection = MoveDirection.Left;
+            }
+        }
+        else if (Mathf.Abs(_horizontalInput) < Mathf.Abs(_verticalInput))
+        {
+            if (_verticalInput > 0)
+            {
+                _movementDirection = MoveDirection.Up;
+            }
+            else if (_verticalInput < 0)
+            {
+                _movementDirection = MoveDirection.Down;
+            }
+        }
+
+        if (_animator)
+        {
+            _animator.SetInteger("MovementDirection", (int)_movementDirection);
+        }
+    }
+    
     private void FixedUpdate()
     {
         if (_rigidbody)
