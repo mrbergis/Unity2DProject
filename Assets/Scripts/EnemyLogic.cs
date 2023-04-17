a7c5a053e93fd240784ed2c5105a03d188a11ed7
mrbergis <mrbergis@mail.ru>
2023-04-18 00:02:18 +0300

    11. Enemy AI & Logics

---
+++ Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState
+{
+    Spawning,
+    Attacking
+}
+public class EnemyLogic : MonoBehaviour
+{
+    EnemyState _enemyState = EnemyState.Spawning;
+
+    GameObject _player;
+
+    Vector2 _attackDirection;
+
+    float _movementSpeed = 5.0f;
+    
+    Rigidbody2D _rigidbody;
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (_enemyState == EnemyState.Attacking)
+        {
+            if(_rigidbody)
+            {
+                _rigidbody.MovePosition(_rigidbody.position + (_attackDirection * _movementSpeed * Time.deltaTime));
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerAttack")
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _attackDirection = -_attackDirection;
+    }
+    
+    public void SetEnemyState(EnemyState enemyState)
+    {
+        _enemyState = enemyState;
+
+        if(_enemyState == EnemyState.Attacking)
+        {
+            DetermineAttackDirection();
+        }
+    }
+
+    void DetermineAttackDirection()
+    {
+        if(!_player)
+        {
+            return;
+        }
+
+        _attackDirection = _player.transform.position - transform.position;
+        _attackDirection.Normalize();
+    }
+}
